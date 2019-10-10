a9c4e2a1a0eaf520584138e2e07ddc2cc83e58aa
Rui Lopes <rgl@ruilopes.com>
2019-10-10 20:03:07 +0100

    upgrade the jenkins helm chart to 1.7.4, jenkins master to 2.190.1 and jenkins slave to 3.35-5

--- examples/jenkins/deploy.sh
+++ examples/jenkins/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-jenkins_helm_version="${1:-1.6.0}"; shift || true # see https://github.com/helm/charts/blob/master/stable/jenkins/Chart.yaml
+jenkins_helm_version="${1:-1.7.4}"; shift || true # see https://github.com/helm/charts/blob/master/stable/jenkins/Chart.yaml
 
 cd $(dirname $0)
 
@@ -9,7 +9,7 @@ cd $(dirname $0)
 # NB this creates the app inside the current rancher cli project (the one returned by rancher context current).
 # see https://github.com/helm/charts/tree/master/stable/jenkins
 # see https://github.com/helm/charts/commits/master/stable/jenkins
-# see https://github.com/helm/charts/tree/b59766d2d37aab540dcb64ee1e0e6c4e0f20f92b/stable/jenkins
+# see https://github.com/helm/charts/tree/868ec67e4300c32040d9ece15fd27054409ef34b/stable/jenkins
 echo "deploying the jenkins app..."
 rancher app install \
     --version $jenkins_helm_version \
